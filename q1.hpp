a735b6ebebe949ef369f797ec2c85597601f32f3
Edriss Osman <exosman0001@zonemail.clpccd.edu>
2023-03-22 18:18:24 -0700

    done

--- q1.hpp
+++ q1.hpp
@@ -5,6 +5,53 @@ using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
+void getinput(int &, int &);
+void getinput (float &, float &);
+void swapTwoValues(int &, int &);
+void swapTwoValues(float &, float &);
+
+
+void getinput(int &n1, int &n2) 
+{
+    cout << "Enter 2 values: " << endl;
+    cin >> n1 >> n2;
+}
+
+void getinput (float &f1, float &f2)
+{
+    cout << "Enter 2 values: " << endl;
+    cin >> f1 >> f2;
+ 
+}
+
+
+void swapTwoValues(int &n1, int &n2)
+{
+    
+    int tap;
+    tap = n1;
+    n1 = n2;
+    n2 = tap;
+
+
+}
+
+
+void swapTwoValues(float &f1, float &f2)
+{
+    
+    float tap;
+    tap = f1;
+    f1 = f2;
+    f2 = tap;
+
+
+}
+
+
+
+
+
 
 // ******************************
 // Implement all your functions here
